e19f4033364f7021ba6ee6b2db9cb57a6b7f1287
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-12 11:51:47 -0500

    fix paths

--- lib/render.php
+++ lib/render.php
@@ -1,15 +1,15 @@
 <?php
 $dir_base =  str_replace('lib', '', __DIR__);
-require $dir_base . '/vendor/autoload.php';
+require $dir_base . 'vendor/autoload.php';
 
 function bs_render($template_file, $data) {
 	$dir_base =  str_replace('lib', '', __DIR__);
-	$loader = new Twig_Loader_Filesystem($dir_base . '/templates');
+	$loader = new Twig_Loader_Filesystem($dir_base . 'templates');
 	$twig = new Twig_Environment($loader, array(
-			'cache' => $dir_base . '/storage/cache'
+			'cache' => $dir_base . 'storage/cache'
 	));
 
-	$template = $twig->load($dir_base . '/' . $template_file);
+	$template = $twig->load($dir_base . $template_file);
 	ob_start();
 	$template->render($data);
 	return ob_get_clean();
