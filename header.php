2f61df3c59f784def3bed703cd2b982f7e6ced4e
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-12 15:01:46 -0500

    add fonts

--- header.php
+++ header.php
@@ -7,6 +7,8 @@
 	<!--insert SEO metas-->
 
 	<title>RentAdvisor</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+	<link href="<?php echo get_template_directory_uri() . '/public/fonts/gotham_rounded.css' ?>" rel="stylesheet">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/public/assets/app.css' ?>">
 </head>
 <body>
