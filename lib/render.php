6f1e8f9f56b52d8c54daf342e30fdef8118e5c94
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-12 11:52:53 -0500

    default data

--- lib/render.php
+++ lib/render.php
@@ -2,7 +2,7 @@
 $dir_base =  str_replace('lib', '', __DIR__);
 require $dir_base . 'vendor/autoload.php';
 
-function bs_render($template_file, $data) {
+function bs_render($template_file, $data = []) {
 	$dir_base =  str_replace('lib', '', __DIR__);
 	$loader = new Twig_Loader_Filesystem($dir_base . 'templates');
 	$twig = new Twig_Environment($loader, array(
