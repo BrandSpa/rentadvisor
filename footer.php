0cbc2868098af1ea96ee7ad4b15e6942ab5babd6
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-12 16:45:37 -0500

    add vendor

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<script src="<?php echo get_template_directory_uri() . '/public/assets/vendor.js' ?>"></script>
 <script src="<?php echo get_template_directory_uri() . '/public/assets/app.js' ?>"></script>
 </body>
 </html>
