2f1de713aef58b5781fa439ad13820b00c7c547a
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-12 17:34:44 -0500

    add normalize

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <?php the_content() ?>
 
-    <div 
+  <div 
       class="bs-headerslider" 
       data-props='{"slides": [
         {
@@ -16,7 +16,7 @@
         }
       ]}'
     >
-    </div>
+  </div>
 
   <?php endwhile; else : ?>
     <h1> <?php echo '404' ?> </h1>
