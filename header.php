ed37aa80537a365a33bb99b82bd0b7dc5a109d14
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-12 14:19:16 -0500

    add link

--- header.php
+++ header.php
@@ -5,7 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<!--insert SEO metas-->
+
 	<title>RentAdvisor</title>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/public/assets/app.css' ?>">
 </head>
 <body>
 
