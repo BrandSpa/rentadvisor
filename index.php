d8aa875dcafae5acd8967574675c028edb959b61
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-12 14:16:36 -0500

    test grid

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 <div id="main">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <?php the_content() ?>
+    <div class="row">
+    <div style="background:red" class="col-xs-12
+              col-sm-8
+              col-md-6
+              col-lg-4">
+      <div class="box">Responsive</div>
+  </div>
+</div>
+
+
   <?php endwhile; else : ?>
     <h1> <?php echo '404' ?> </h1>
   <?php endif; ?>
