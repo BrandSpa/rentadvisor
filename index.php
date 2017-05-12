22429812dc075edd6934d86f67c722a406b5c0c4
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-12 14:20:12 -0500

    #FC3481

--- index.php
+++ index.php
@@ -9,6 +9,12 @@
               col-lg-4">
       <div class="box">Responsive</div>
   </div>
+      <div style="background:#FC3481" class="col-xs-12
+              col-sm-8
+              col-md-6
+              col-lg-4">
+      <div class="box">Responsive</div>
+  </div>
 </div>
 
 
