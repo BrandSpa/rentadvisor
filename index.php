f0903a3847a8e204a51cf09c160e020d95e4af57
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-15 17:04:48 -0500

    add width

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             "bg": "#295AB7",
             "img": "<?php echo get_template_directory_uri() ?>/public/img/woman.png",
             "col": "col-lg-7",
-            "width": "800px"
+            "width": "600px"
           }
         },
          {
@@ -31,7 +31,8 @@
           "sectionRight": {
             "bg": "#322365",
             "img": "<?php echo get_template_directory_uri() ?>/public/img/laptop.png",
-            "col": "col-lg-8" 
+            "col": "col-lg-8",
+            "width": "600px"
           }
         }
       ]}'
